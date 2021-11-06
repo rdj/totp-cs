f3c76874d22179e6dec90f404040317a828e7156
Ryan D Johnson <ryandjohnson@gmail.com>
2021-11-06 11:48:37 -0700

    Basic version, uses hex key stored in hardcoded credman cred

---
+++ totp/CredApi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totp
+{
+    static class CredApi
+    {
+        enum CRED_TYPE : int
+        {
+            GENERIC = 1,
+            DOMAIN_PASSWORD = 2,
+            DOMAIN_CERTIFICATE = 3,
+            DOMAIN_VISIBLE_PASSWORD = 4,
+            MAXIMUM = 5
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        struct CREDENTIAL
+        {
+            public int flags;
+            public int type;
+            public IntPtr targetName;
+            public IntPtr comment;
+            public System.Runtime.InteropServices.ComTypes.FILETIME lastWritten;
+            public int credentialBlobSize;
+            public IntPtr credentialBlob;
+            public int persist;
+            public int attributeCount;
+            public IntPtr credAttribute;
+            public IntPtr targetAlias;
+            public IntPtr userName;
+        }
+
+        [DllImport("advapi32.dll", EntryPoint = "CredReadW", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern bool CredRead(string target, CRED_TYPE type, int reservedFlag, out IntPtr credentialPtr);
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        static extern bool CredFree([In] IntPtr buffer);
+
+        internal static string getPassword(string serviceName)
+        {
+            IntPtr pCred = IntPtr.Zero;
+            try {
+                if ( !CredRead(serviceName, CRED_TYPE.GENERIC, 0, out pCred) ) {
+                    throw new Win32Exception(Marshal.GetLastWin32Error(), "Error from CredRead");
+                }
+                var cred = Marshal.PtrToStructure<CREDENTIAL>(pCred);
+                return Marshal.PtrToStringUni(cred.credentialBlob, cred.credentialBlobSize / 2);
+            }
+            finally {
+                if ( IntPtr.Zero != pCred ) {
+                    CredFree(pCred);
+                    pCred = IntPtr.Zero;
+                }
+            }
+        }
+    }
+}
--- totp/Program.cs
+++ totp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,35 @@ namespace totp
 {
     class Program
     {
+        static byte[] HexStringToBytes(string hex)
+        {
+            if ( String.IsNullOrEmpty(hex) || 0 != hex.Length % 2 ) {
+                throw new ApplicationException("Bad hex string");
+            }
+            var result = new byte[hex.Length / 2];
+            for ( int i = 0; i < hex.Length / 2; i++ ) {
+                result[i] = Convert.ToByte(hex.Substring(2*i, 2), 16);
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            var elapsedSeconds = DateTime.UtcNow.Subtract(epoch).TotalSeconds;
+            var counter = (long)elapsedSeconds / 30;
+            var keyString = CredApi.getPassword("ffxiv-totp");
+            var key = HexStringToBytes(keyString);
+            var counterBytesBE = BitConverter.GetBytes(IPAddress.HostToNetworkOrder(counter));
+            var hmac = new HMACSHA1(key).ComputeHash(counterBytesBE);
+            var offset = (uint)hmac[19] & 0xf;
+            var binCode = (UInt32)(
+                (hmac[offset] & 0x7f) << 24
+                | (hmac[offset + 1] & 0xff) << 16
+                | (hmac[offset + 2] & 0xff) << 8
+                | (hmac[offset + 3] & 0xff));
+            var totp = binCode % 1_000_000;
+            Console.WriteLine("{0:D6}", totp);
         }
     }
 }
